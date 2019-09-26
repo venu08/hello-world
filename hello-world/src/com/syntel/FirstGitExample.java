c6b31d0d8776b0dbf04b6ffef1318407babf4090
venu08 <venugopala_reddygoli@syntelinc.com>
2019-09-26 14:25:47 +0530

    second commit

--- hello-world/src/com/syntel/FirstGitExample.java
+++ hello-world/src/com/syntel/FirstGitExample.java
@@ -3,7 +3,7 @@ package com.syntel;
 public class FirstGitExample {
 
 	public static void main(String[] args) {
-		System.out.println("First commit");
+		System.out.println("Second commit");
 	}
 
 }
